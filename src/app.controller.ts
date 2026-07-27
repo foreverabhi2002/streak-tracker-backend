b0862ae08f07316cd26d1df044969f03ff8ee3c5
Abhishek Mahato <abhishekmahato2002@gmail.com>
2026-07-28 01:06:56 +0530

    landing page fixed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Header } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   @Header('Content-Type', 'text/html')
   @ApiOperation({ summary: 'Get Welcome Page' })
   getHello(): string {
